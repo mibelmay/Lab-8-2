d1402d46c223ec977bfb5e76bf06767a0dcb2213
Stacy <zemcovannastasia11@gmail.com>
2022-12-13 14:53:24 +0500

    добавила класс операций и класс LoadActions

---
+++ BankAccount.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+
+    class Operation
+    {
+        public int Date { get; private set; }
+        public int Time { get; private set; }
+        public int Amount { get; private set; }
+        public string Action { get; private set; }
+
+        public Operation(int date, int time, int amount, string action)
+        {
+            Date = date;
+            Time = time;
+            Amount = amount;
+            Action = action;
+        }
+
+        public Operation(int date, int time, string action)
+        {
+            Date = date;
+            Time = time;
+            Action = action;
+        }
+
+        public int In()
+        {
+            return Amount;
+        }
+
+        public int Out()
+        {
+            return Amount * -1;
+        }
+
+        public override string ToString()
+        {
+            return $"{Date}, {Time}, {Amount}, {Action}";
+        }
+    }
+    class LoadActions
+    {
+        public static Operation[] LoadBankAccount(string[] file)
+        {
+            Operation[] result = new Operation[file.Length - 1];
+            for (int i = 1; i < file.Length; i++)
+            {
+                string[] line = file[i].Split('|');
+                line[0].Trim();
+                int date = int.Parse((String.Join("", (line[0].Split(' '))[0].Split('-'))));
+                int time = int.Parse((String.Join("", (line[0].Split(' '))[1].Split(':'))));
+
+                line[1].Trim();
+                if (line.Length == 2)
+                {
+
+                    result[i - 1] = new Operation(date, time, line[1]);
+                    continue;
+                }
+                
+                int amount = int.Parse(line[1]);
+                string action = line[2].Trim();
+                result[i - 1] = new Operation(date, time, amount, action);
+
+            }
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Principal;
+
+namespace BankAccount
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            int initialAccount;
+            string path = Environment.CurrentDirectory.ToString() + "/account.txt";
+            string[] file = File.ReadAllLines(path);
+            initialAccount = Convert.ToInt32(file[0]);
+            Operation[] operations = LoadActions.LoadBankAccount(file);
+
+            foreach (Operation operation in operations)
+                Console.WriteLine(operation);
+        }
+    }
+}
