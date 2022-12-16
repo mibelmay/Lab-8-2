58643eab12949d5bb0f2f8e2a6000b88e32e0cd8
Stacy <zemcovannastasia11@gmail.com>
2022-12-16 23:34:25 +0500

    пофиксила

---
+++ Operation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankAccount
+{
+    class Operation
+    {
+        public DateTime Date { get; private set; }
+        public int Amount { get; private set; }
+        public string Action { get; private set; }
+
+        public Operation(DateTime date, int amount, string action)
+        {
+            Date = date;
+            Amount = amount;
+            Action = action;
+        }
+
+        public Operation(DateTime date, string action)
+        {
+            Date = date;
+            Action = action;
+        }
+
+    }
+}
